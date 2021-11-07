3d1e3d95cc42ad70f264965ed5c8916530915576
Andre Racz <andre.racz@avanade.com>
2021-11-07 19:23:15 -0300

    Get data

--- function/servers/index.ts
+++ function/servers/index.ts
@@ -1,17 +1,33 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { TableClient } from "@azure/data-tables"
+import { DefaultAzureCredential } from "@azure/identity"
+
+const account = process.env["TABLE_ACCOUNT"];
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
+    const credential = new DefaultAzureCredential();
+    const tableClient = new TableClient(
+        `https://${account}.table.core.windows.net`,
+        'servers',
+        credential
+      );
+
+    let entitiesIter = tableClient.listEntities();
+    let resp:any = [];
+    for await (const entity of entitiesIter) {
+      resp.push(await getServerInfo(entity));
+    }
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-
+        body: resp
+      };
 };
+
+async function getServerInfo(serverInfo) {
+    return {
+        serverName: serverInfo.rowKey, size: serverInfo.size, maxPlayers: serverInfo.maxPlayers
+    }
+}
 
 export default httpTrigger;
